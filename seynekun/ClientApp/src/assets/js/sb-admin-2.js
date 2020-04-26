9f743170fd2c11098ff217ec925592113a62d268
itskevinn <kevinponton@unicesar.edu.co>
2020-04-26 15:15:21 -0500

    arreglo de los botones en nav

--- seynekun/ClientApp/src/assets/js/sb-admin-2.js
+++ seynekun/ClientApp/src/assets/js/sb-admin-2.js
@@ -10,19 +10,6 @@
     };
   });
 
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
     if ($(window).width() < 768) {
