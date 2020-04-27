8eb68e0becb030aa7b24c4a4348751293112dad0
Muchacho <mvilladiegos@unicesar.edu.co>
2020-04-26 20:38:38 -0500

    Pequeño cambio en consultar
    Productor puede consultar y guardar, confirmo

--- seynekun/ClientApp/src/app/seynekun/productor/productor-consulta/productor-consulta.component.ts
+++ seynekun/ClientApp/src/app/seynekun/productor/productor-consulta/productor-consulta.component.ts
@@ -18,34 +18,9 @@ export class ProductorConsultaComponent implements OnInit {
   ngOnInit(): void {        
     this.productorService.gets().subscribe(result => {
       this.productores = result;
-      this.productores = [{
-        nombre: "Kevin",
-        nombrePredio: "La Camita",
-        apellido: "Pontón",
-        numeroTelefono: "3213213214",
-        cedulaCafetera: "1AB3",
-        cedula: "119322",
-        afiliacionSalud: "Sisben",
-        codigoFinca: "K01P",
-        codigoSica: "123Pnnt",
-        municipio: "Valledupar",
-        vereda: "Mareigua"
-      },
-      {
-        nombre: "Juan",
-        nombrePredio: "La Casita",
-        apellido: "Ortiz",
-        numeroTelefono: "3222222222",
-        cedulaCafetera: "1JO3",
-        cedula: "Juank",
-        afiliacionSalud: "Salud",
-        codigoFinca: "J01O",
-        codigoSica: "123Orrt",
-        municipio: "Pueblo Bello",
-        vereda: "Casco Urbano"
-      }]
     });    
   }
+
   alDigitar(event: any) {
     this.validarTamañoLista();
     }
