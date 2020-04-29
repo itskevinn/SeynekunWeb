7c70262c6d4f1f3c534dff6875cfe1c9153de477
itskevinn <kevinponton@unicesar.edu.co>
2020-04-29 15:14:25 -0500

    creacion de algunos componentes para inventario

--- seynekun/ClientApp/src/app/app-routing.module.ts
+++ seynekun/ClientApp/src/app/app-routing.module.ts
@@ -1,29 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { LoteRegistroComponent } from './seynekun/lote/lote-registro/lote-registro.component';
-import { LoteConsultaComponent } from './seynekun/lote/lote-consulta/lote-consulta.component';
-import { ProductorRegistroComponent } from './seynekun/productor/productor-registro/productor-registro.component';
-import { ProductorConsultaComponent } from './seynekun/productor/productor-consulta/productor-consulta.component';
-import { ProductorEdicionComponent } from './seynekun/productor/productor-edicion/productor-edicion.component';
-import { EmpleadoConsultaComponent } from './seynekun/empleado/empleado-consulta/empleado-consulta.component';
-import { EmpleadoRegistroComponent } from './seynekun/empleado/empleado-registro/empleado-registro.component';
-import { EmpleadoEdicionComponent } from './seynekun/empleado/empleado-edicion/empleado-edicion.component';
-import { ClienteConsultaComponent } from './seynekun/cliente/cliente-consulta/cliente-consulta.component';
-import { ClienteEdicionComponent } from './seynekun/cliente/cliente-edicion/cliente-edicion.component';
-import { ClienteRegistroComponent } from './seynekun/cliente/cliente-registro/cliente-registro.component';
-import { EmpleadoVistaComponent } from './seynekun/empleado/empleado-vista/empleado-vista.component';
-import { ClienteVistaComponent } from './seynekun/cliente/cliente-vista/cliente-vista.component';
-import { ProductorVistaComponent } from './seynekun/productor/productor-vista/productor-vista.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Routes, RouterModule } from '@angular/router'
+import { LoteRegistroComponent } from './seynekun/lote/lote-registro/lote-registro.component'
+import { LoteConsultaComponent } from './seynekun/lote/lote-consulta/lote-consulta.component'
+import { ProductorRegistroComponent } from './seynekun/productor/productor-registro/productor-registro.component'
+import { ProductorConsultaComponent } from './seynekun/productor/productor-consulta/productor-consulta.component'
+import { ProductorEdicionComponent } from './seynekun/productor/productor-edicion/productor-edicion.component'
+import { EmpleadoConsultaComponent } from './seynekun/empleado/empleado-consulta/empleado-consulta.component'
+import { EmpleadoRegistroComponent } from './seynekun/empleado/empleado-registro/empleado-registro.component'
+import { EmpleadoEdicionComponent } from './seynekun/empleado/empleado-edicion/empleado-edicion.component'
+import { ClienteConsultaComponent } from './seynekun/cliente/cliente-consulta/cliente-consulta.component'
+import { ClienteEdicionComponent } from './seynekun/cliente/cliente-edicion/cliente-edicion.component'
+import { ClienteRegistroComponent } from './seynekun/cliente/cliente-registro/cliente-registro.component'
+import { EmpleadoVistaComponent } from './seynekun/empleado/empleado-vista/empleado-vista.component'
+import { ClienteVistaComponent } from './seynekun/cliente/cliente-vista/cliente-vista.component'
+import { ProductorVistaComponent } from './seynekun/productor/productor-vista/productor-vista.component'
+import { ProductoConsultaComponent } from './seynekun/inventario/producto/producto-consulta/producto-consulta.component'
+import { ProductoEdicionComponent } from './seynekun/inventario/producto/producto-edicion/producto-edicion.component'
+import { ProductoRegistroComponent } from './seynekun/inventario/producto/producto-registro/producto-registro.component'
+import { ProductoVistaComponent } from './seynekun/inventario/producto/producto-vista/producto-vista.component'
+import { ReporteInventarioComponent } from './seynekun/inventario/reporte-inventario/reporte-inventario.component'
 
 const routes: Routes = [
   {
     path: 'RegistrarLote',
-    component: LoteRegistroComponent
+    component: LoteRegistroComponent,
   },
   {
     path: 'ConsultarLote',
-    component: LoteConsultaComponent
+    component: LoteConsultaComponent,
   },
   { path: 'RegistrarProductor', component: ProductorRegistroComponent },
   { path: 'Productores', component: ProductorConsultaComponent },
@@ -37,14 +42,16 @@ const routes: Routes = [
   { path: 'RegistrarCliente', component: ClienteRegistroComponent },
   { path: 'EditarCliente/:id', component: ClienteEdicionComponent },
   { path: 'Cliente/:id', component: ClienteVistaComponent },
+  { path: 'Productos', component: ProductoConsultaComponent },
+  { path: 'Producto/:id', component: ProductoVistaComponent },
+  { path: 'EditarProducto/:id', component: ProductoEdicionComponent },
+  { path: 'RegistrarProducto', component: ProductoRegistroComponent },
+  { path: 'ValorInventario', component: ReporteInventarioComponent },
 ]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ seynekun/ClientApp/src/app/seynekun/inventario/producto/producto-consulta/producto-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-consulta',
+  templateUrl: './producto-consulta.component.html',
+  styleUrls: ['./producto-consulta.component.css']
+})
+export class ProductoConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
