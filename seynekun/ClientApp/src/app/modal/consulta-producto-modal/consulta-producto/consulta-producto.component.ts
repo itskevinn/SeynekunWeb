ebd0d48e4ee5a067ccd5f08e44c069fb10254277
itskevinn <kevinponton@unicesar.edu.co>
2020-06-14 03:18:58 -0500

    Producción bien maracatón

--- seynekun/ClientApp/src/app/modal/consulta-producto-modal/consulta-producto/consulta-producto.component.ts
+++ seynekun/ClientApp/src/app/modal/consulta-producto-modal/consulta-producto/consulta-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/servicios/servicio-producto/producto.service';
+import { EventoService } from 'src/app/servicios/servicio-evento/evento.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Producto } from 'src/app/seynekun/models/modelo-producto/producto';
 
 @Component({
   selector: 'app-consulta-producto',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaProductoComponent implements OnInit {
 
-  constructor() { }
+  productos: Producto[];
+  cliente: Producto;
+  listaVacia: Boolean = true;
+  cantidadproductos: Number;
+  textoABuscar: String;
+  constructor(private productoService: ProductoService, private eventoServicio: EventoService, public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    this.productoService.gets().subscribe(result => {
+      this.productos = result;
+    });
   }
-
+  enviarId(id: string) {
+    this.eventoServicio.cambiarId(id);
+    console.log(id)
+  }
 }
